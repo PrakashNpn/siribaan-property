79d610721a5e6f981c46b6fb8a3e1ae99399c4ca
thihan <thihan839@gmail.com>
2026-06-15 15:39:50 +0700

    Disable Vercel Image Optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ['pdf-parse'],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
